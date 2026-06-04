07f4c93417a6dced06f42baac8c41409ae37049a
credelsamck <credelsamck@gmail.com>
2026-06-04 12:59:23 +0100

    appprovid

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,19 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Artisan;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        if (env('APP_ENV') === 'production') {
+            Artisan::call('migrate', ['--force' => true]);
+        }
     }
 }
