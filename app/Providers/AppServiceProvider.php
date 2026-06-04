1599b57950d44508be70c518e8d39e5fd94f2116
credelsamck <credelsamck@gmail.com>
2026-06-04 13:02:07 +0100

    retour

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Artisan;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,8 +13,6 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        if (env('APP_ENV') === 'production') {
-            Artisan::call('migrate', ['--force' => true]);
-        }
+        //
     }
-}
+}r
